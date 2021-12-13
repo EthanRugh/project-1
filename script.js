9282ff49529b1f0a51f21f27f42efb2b37f7d9d4
brenhamp <bpozzi@trinity.edu>
2021-12-13 15:42:58 -0600

    initial functionality of API; working on displaying video

---
+++ script.js
@@ -0,0 +1,37 @@
+filenameEl = document.getElementById("filename");
+episodeEl = document.getElementById("episode");
+imageEl = document.getElementById("image");
+videoEl = document.getElementById("video");
+
+function imageSearch(form) {
+  let queryURL = "https://api.trace.moe/search";
+  var settings = {
+    "url": queryURL,
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "mimeType": "multipart/form-data",
+    "contentType": false,
+    "data": form
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  })
+  .done(function (response) {
+      const jsonData = JSON.parse(response);
+      filenameEl.innerHTML = "Show: " + jsonData.result[0].filename;
+      episodeEl.innerHTML = "Episode: " + jsonData.result[0].episode;
+      document.getElementById("image").src = jsonData.result[0].image; 
+      document.getElementById("video").src = jsonData.result[0].video; 
+  });
+}
+
+$("#reverse-img-form").submit(function(e){
+  e.preventDefault();
+  let formdata = null;
+  if (window.FormData){
+    formdata = new FormData($(this)[0]);
+  }
+  imageSearch(formdata);
+});
