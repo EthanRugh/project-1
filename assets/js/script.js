8f9fb72191ac93795c65cfd5d8cbe14255038e0a
brenhamp <bpozzi@trinity.edu>
2021-12-14 20:25:44 -0600

    fixed text search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,45 @@ let searchBar = document.querySelector("#form-search");
 let searchBtn = document.querySelector("#search-btn");
 let infoHold = document.querySelector("#info-hold");
 
+let getAnime = function(animeShow) {
+    var searchApiUrl = "https://api.jikan.moe/v3/search/anime?q=" + animeShow;
+
+    fetch(searchApiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                infoHandler(data);
+            })
+        }
+    })
+}
+
+let infoHandler = function(data) {
+    console.log(data);
+    let title = document.createElement("h3")
+    title.textContent = "Title: " + data.results[0].title
+    let rating = document.createElement("p")
+    rating.textContent = "Rating: " + data.results[0].rated 
+    let epi = document.createElement("p")
+    epi.textContent = "Episodes: " + data.results[0].episodes
+    let score = document.createElement("p")
+    score.textContent = "Overall Score: " + data.results[0].score
+    let synops = document.createElement("p")
+    synops.textContent = "Description: " + data.results[0].synopsis
+
+    infoHold.appendChild(title);
+    infoHold.appendChild(rating);
+    infoHold.appendChild(epi);
+    infoHold.appendChild(score);
+    infoHold.appendChild(synops);
+}
+
+$("#title-search").submit(function(event){ {
+    event.preventDefault();
+    let animeShow = searchBar.value.trim();
+    getAnime(animeShow);
+}
+})
+
 function imageSearch(form) {
   let queryURL = "https://api.trace.moe/search";
   var settings = {
@@ -40,41 +79,3 @@ $("#reverse-img-file").submit(function(e){
   }
   imageSearch(formdata);
 });
-
-let getAnime = function(animeShow) {
-    var searchApiUrl = "https://api.jikan.moe/v3/search/anime?q=" + animeShow;
-
-    fetch(searchApiUrl).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                infoHandler(data);
-            })
-        }
-    })
-}
-
-let infoHandler = function(data) {
-    console.log(data);
-    let title = document.createElement("h3")
-    title.textContent = "Title: " + data.results[0].title
-    let rating = document.createElement("p")
-    rating.textContent = "Rating: " + data.results[0].rated 
-    let epi = document.createElement("p")
-    epi.textContent = "Episodes: " + data.results[0].episodes
-    let score = document.createElement("p")
-    score.textContent = "Overall Score: " + data.results[0].score
-    let synops = document.createElement("p")
-    synops.textContent = "Description: " + data.results[0].synopsis
-
-    infoHold.appendChild(title);
-    infoHold.appendChild(rating);
-    infoHold.appendChild(epi);
-    infoHold.appendChild(score);
-    infoHold.appendChild(synops);
-}
-
-var searchBarHandler = function(event) {
-    event.preventDefault();
-    let animeShow = searchBar.value.trim();
-    getAnime(animeShow);
-}
