4dc0d33fa8aea6b788a0cf7ade33f805846de529
Isaac yanez <isaac231467@gmail.com>
2021-12-15 20:36:12 -0600

    added CSS to the landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,40 +5,11 @@ videoEl = document.getElementById("video");
 let searchBar = document.querySelector("#form-search");
 let searchBtn = document.querySelector("#search-btn");
 let infoHold = document.querySelector("#info-hold");
-
-function imageSearch(form) {
-  let queryURL = "https://api.trace.moe/search";
-  var settings = {
-    "url": queryURL,
-    "method": "POST",
-    "timeout": 0,
-    "processData": false,
-    "mimeType": "multipart/form-data",
-    "contentType": false,
-    "data": form
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  })
-  .done(function (response) {
-      const jsonData = JSON.parse(response);
-      filenameEl.innerHTML = "Show: " + jsonData.result[0].filename;
-      episodeEl.innerHTML = "Episode: " + jsonData.result[0].episode;
-      imageEl.src = jsonData.result[0].image + "&size=l"; 
-      videoEl.src = jsonData.result[0].video + "&size=l"; 
-      videoEl.classList.remove("d-none");
-  })
-}
-
-$("#reverse-img-file").submit(function(e){
-  e.preventDefault();
-  let formdata = null;
-  if (window.FormData){
-    formdata = new FormData($(this)[0]);
-  }
-  imageSearch(formdata);
-});
+let title = document.createElement("h3");
+let rating = document.createElement("p")
+let epi = document.createElement("p");
+let score = document.createElement("p");
+let synops = document.createElement("p");
 
 let getAnime = function(animeShow) {
     var searchApiUrl = "https://api.jikan.moe/v3/search/anime?q=" + animeShow;
@@ -53,16 +24,11 @@ let getAnime = function(animeShow) {
 }
 
 let infoHandler = function(data) {
-    console.log(data);
-    let title = document.createElement("h3")
+    
     title.textContent = "Title: " + data.results[0].title
-    let rating = document.createElement("p")
     rating.textContent = "Rating: " + data.results[0].rated 
-    let epi = document.createElement("p")
     epi.textContent = "Episodes: " + data.results[0].episodes
-    let score = document.createElement("p")
     score.textContent = "Overall Score: " + data.results[0].score
-    let synops = document.createElement("p")
     synops.textContent = "Description: " + data.results[0].synopsis
 
     infoHold.appendChild(title);
@@ -72,8 +38,47 @@ let infoHandler = function(data) {
     infoHold.appendChild(synops);
 }
 
-var searchBarHandler = function(event) {
+$("#title-search").submit(function(event){ {
     event.preventDefault();
     let animeShow = searchBar.value.trim();
+    title.textContent = "";
+    rating.textContent = "";
+    epi.textContent = "";
+    score.textContent = "";
+    synops.textContent = "";
     getAnime(animeShow);
 }
+})
+
+function imageSearch(form) {
+  let queryURL = "https://api.trace.moe/search";
+  var settings = {
+    "url": queryURL,
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "mimeType": "multipart/form-data",
+    "contentType": false,
+    "data": form
+  };
+  
+  $.ajax(settings).done(function (response) {
+  })
+  .done(function (response) {
+      const jsonData = JSON.parse(response);
+      filenameEl.innerHTML = "Show: " + jsonData.result[0].filename;
+      episodeEl.innerHTML = "Episode: " + jsonData.result[0].episode;
+      imageEl.src = jsonData.result[0].image + "&size=m"; 
+      videoEl.src = jsonData.result[0].video + "&size=m"; 
+      videoEl.classList.remove("d-none");
+  })
+}
+
+$("#reverse-img-file").submit(function(e){
+  e.preventDefault();
+  let formdata = null;
+  if (window.FormData){
+    formdata = new FormData($(this)[0]);
+  }
+  imageSearch(formdata);
+});
